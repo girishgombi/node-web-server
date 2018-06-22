e0e709f3d41f329a2c0e74d8f8d08ebaca91ea1a
Girish Gombi <girishgombi@girishs-mbp.attlocal.net>
2018-06-21 22:57:14 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('getFullYear', ()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (message)=>{
+  return message.toUpperCase();
+});
+
+app.use((req, res, next)=>{
+  var now = new Date().toString();
+  var logs = `${now} ${req.method} ${req.url}`;
+  console.log(logs);
+  fs.appendFile('server.log', logs + '\n',(error)=>{
+    if(error){
+      console.log(error);
+    }
+  });
+  next();
+});
+
+app.use((req, res, next)=>{
+  res.render('maintenence.hbs')
+});
+
+app.get('/', (req, res) =>{
+//res.send('<h1> Hi Express on node <h1>')
+//app.use('/static', express.static(path.join(__dirname, 'public')));
+/*
+res.send(
+      {
+        name : "Some Name",
+        age  : "Age",
+        like : [
+          "Cricket",
+          "golf"
+        ]
+      }
+    )
+    */
+
+    res.render('home.hbs', {
+      pageTitle : 'Home Page',
+      welcomeMsg : 'Welcome Guest ..!!!'
+    })
+});
+
+app.get('/about', (req,res)=>{
+  res.render('about.hbs',{
+    pageTitle : 'About Page'
+  })
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+
+        errorcode : "123",
+        errorMessage : "Some Error Msg",
+        errorDesc : "Error Description"
+
+  })
+})
+
+app.listen(3000,()=>{
+  console.log("server is up and running on port 3000...");
+});
